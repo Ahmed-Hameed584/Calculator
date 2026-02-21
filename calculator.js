b0c7ecd6fa801d644f68ed233e97686c6686be8a
Ahmed-Hameed584 <ahmed399332@gmail.com>
2026-02-21 22:20:16 +0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const display = document.querySelector('.display');
+function appendToDisplay(value) {
+    display.value += value;
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
